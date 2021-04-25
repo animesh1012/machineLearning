bf49787420b537242a8c0a6fbebf00fe0bb7d9a7
Animesh Tripathi <59251365+animesh1012@users.noreply.github.com>
2021-04-25 15:33:46 +0530

    Add files via upload

---
+++ Face_Eye_Smile_Detection/self_done_homework.py
@@ -0,0 +1,36 @@
+# Importing Libraries
+import cv2
+
+# Loading Cascade
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
+
+# Making function for recognising smile
+def detect(gray, frame):
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 22)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+        smiles = smile_cascade.detectMultiScale(roi_gray, 1.7, 25)
+        for (sx, sy, sw, sh) in smiles:
+            cv2.rectangle(roi_color, (sx, sy), (sx+sw, sy+sh), (0, 0, 255), 2)
+    return frame
+
+# Doing Smile Recognition with webcam
+video_capture  = cv2.VideoCapture(0)
+while(True):
+    _,frame = video_capture.read()
+    gray = cv2.cvtColor(frame,cv2.COLOR_RGB2GRAY)
+    canvas = detect(gray,frame)
+    cv2.imshow('Video',canvas)
+    if cv2.waitKey(1) & 0xFF==ord('q'):
+        break
+video_capture.release()
+cv2.destroyAllWindows()
+
+    
